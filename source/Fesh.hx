fd4c147b4f786605d66147ceafd21d78893db7a7
Diego <fonseca.diego@outlook.com>
2022-08-17 04:50:12 -0400

    Info

--- source/Fesh.hx
+++ source/Fesh.hx
@@ -3,6 +3,9 @@ package;
 import lime.system.System;
 
 class Fesh {
+    /**
+    * WARNING: Changing this can lead to issues with the physics system.
+    */
     public static var useFixedTimestep:Bool = true;
 
     /**
