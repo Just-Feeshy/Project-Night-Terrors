559587498439d33abb205367fba131713b55a444
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-08-24 19:54:01 -0400

    for html and javascript

--- source/backend/NativeExtensions.hx
+++ source/backend/NativeExtensions.hx
@@ -1,9 +1,13 @@
 package backend;
 
+#if cpp
 @:include("BackendHelper.cpp")
 @:keep
 @:native("feeshmora::BackendHelper")
+#end
 extern class NativeExtensions {
+    #if cpp 
     @:native("feeshmora::BackendHelper::init")
+    #end
 	public static function init():Void;
 }
