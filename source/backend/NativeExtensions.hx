f50060a38a21968181dde23a2ca650fea7602de0
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-08-24 19:55:00 -0400

    new metadatas

--- source/backend/NativeExtensions.hx
+++ source/backend/NativeExtensions.hx
@@ -2,9 +2,10 @@ package backend;
 
 #if cpp
 @:include("BackendHelper.cpp")
-@:keep
+@:unreflective
 @:native("feeshmora::BackendHelper")
 #end
+@:keep
 extern class NativeExtensions {
     #if cpp 
     @:native("feeshmora::BackendHelper::init")
